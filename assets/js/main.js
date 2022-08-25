a7ad89b722904711e375c94f447a51ccba01f48a
KatKlee <katklee2022@gmail.com>
2022-08-25 14:31:46 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,14 @@
 /* console.log('Start Warten für 3 Sekunden…')
 setTimeout(function task1() { console.log('Erledigt. Du hast 3 Sekunden verschwendet.') }, 3000) */
 
+
 // task: Write a function that displays the result of the given preview. Use setInterval and new Date().
 
 /* setInterval(function task2() { console.log(new Date().toLocaleTimeString()) }, 1000) */
 
+
 // task: Write a function that displays the result of the given preview. Use setInterval, clearInterval and if, else.
+
 let counter = 10
 let myInterval = setInterval(task3, 1000)
 
